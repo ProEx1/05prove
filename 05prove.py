81ab3e19d38fc4abae18737916ef288b206af8a2
ProEx1 <115039858+ProEx1@users.noreply.github.com>
2022-10-14 14:00:33 -0500

    Update 05prove.py

--- 05prove.py
+++ 05prove.py
@@ -35,7 +35,7 @@ if answer1.lower() == "north":
             print("Everything is going to be alright. ")
             print(dash)
         elif answer1_north_close_barn.lower() == "no":
-            print("Thoughts of hunger overwhelm your senses and you wake up in your own bed. \nWhat a strange dream! ")
+            print("Thoughts of hunger overwhelm your senses \nand you wake up in your own bed. \nWhat a strange dream! ")
             print(dash)
         else:
             print("You have entered an incorrect response. \nPlease restart the program. ")
@@ -84,7 +84,7 @@ elif answer1.lower() == "west":
         #print(dash)
     elif answer1_west_lights.lower() == "yes":
         print("You blindly run at the lights but as you reach them \nthey all go out at once leaving you confused.")
-        print("Thoughts of hunger overwhelm your senses and you wake up in your own bed. \nWhat a strange dream! ")
+        print("Thoughts of hunger overwhelm your senses \nand you wake up in your own bed. \nWhat a strange dream! ")
     else:
         print("You have entered an incorrect response. \nPlease restart the program. ")
     print(dash)
@@ -92,7 +92,57 @@ elif answer1.lower() == "west":
 
 #EAST - DARKNESS
 elif answer1.lower() == "east":
-    print("You turn and slowly move down the path to the east, taking in the calm air as you go. ")
+    print("You turn and slowly move down the path to the east, \ntaking in the calm air as you go. ")
+    print("The trees tower and curl over you \nlike an ocean wave ready to break. ")
+    print("The air gets thicker and thicker the further in you go. ")
+    print("You start to ponder if you already had claustiphobia \nor if this is a new feeling. ")
+    print(dash)
+    answer1_fire = input("As the sun gets lower the forest \ngets too dark to see the trail. \nYou bump into a pile of wood, \ndo you want to try to build a fire? YES or NO? ")
+    print(dash)
+    if answer1_fire.lower() == "yes":
+        print("In the dark you stumble around and put together \nsome logs and kindlin. \nWhile blindly reaching around, you find a box of matches. ")
+        print("The flame is somehow darker than the \nsurrounding area and you feel even colder. ")
+        print(dash)
+        answer1_fire_fire2 = input("Do you want to try to EXTINGUISH the fire or STAY? ")
+        print(dash)
+        if answer1_fire_fire2.lower() == "extinguish":
+            print("You scramble the logs and air rushes into where the flame was. \nYou hear a loud bang and are pushed back. ")
+            print("You wake up in your own bed. \nWhat a strange dream! ")
+        elif answer1_fire_fire2.lower() == "stay":
+            print("You lay down and cover yourself with some leaves to stay warm, \nNot noticing the darkness of the flame is slowly spreading. ")
+            print("The absolute darkness comsumes everything, including you. ")
+            print("You wake up in your own bed. \nWhat a strange dream! ")
+        else:
+            print("You have entered an incorrect response. \nPlease restart the program. ")
+        print(dash)
+    elif answer1_fire.lower() == "no":
+        print("You press on, the air smells foul, and the \nground below you feels more spongey. ")
+        print("You feel water entering your shoes, \nyou can't see it, but you're sure you're in a swamp. ")
+        print(dash)
+        answer1_fire_water = input("The water is up to your knees now. Do you turn back? YES or NO? ")
+        print(dash)
+        if answer1_fire_water.lower() == "yes":
+            print("You start to turn back but the swamp does not let go. ")
+            print("You begin sinking, slowly at first, \nand faster as you struggle more. ")
+            print("As the water starts to get to you face \nyou feel overcome with terror. ")
+            print("You wake up in your own bed. \nWhat a strange dream! ")
+        elif answer1_fire_water.lower() == "no":
+            print("You press on, feeling the weight of the water \nand soft sand pulling you down. ")
+            print("You see a light in the distance, \nit fills you with hope. ")
+            print("You focus on the light as you keep \ntelling yourself it's just a bit further. ")
+            print("In the light you can make out a large building. ")
+            print("As you get closer you see a towering \nwhite building with gold windows. ")
+            print("You make it to shore and pull yourself out \nof the water and mud. ")
+            print('You see a sign that says, \n"Welcome to LAuberge Casino" ')
+            print("You're horrified to realize you ended up in Louisiana. ")
+        else:
+            print("You have entered an incorrect response. \nPlease restart the program. ")
+        print(dash)
+            
+    else:
+        print("You have entered an incorrect response. \nPlease restart the program. ")
+    #print(dash)
+
 
 
 else:
